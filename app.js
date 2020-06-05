509866538ae8d3948992b576ddec885861adb54a
patrickfinnigan <patrickfinnigan94@gmail.com>
2020-06-05 01:52:08 -0700

    learning api routes

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
-const express = require ("express");
+const express = require("express");
+const path = require("path");
+const exphbs = require("express-handlebars");
+const logger = require("./middleware/logger");
 
 const app = express();
+
+//Init middleware
+// app.use(logger);
+
+//Handlebars Middleware
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+// Body Parser Middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: false}))
+
+// Set a static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Members API Routes
+app.use('/api/members', require('./routes/api/members'))
 
 const PORT = process.env.PORT || 5000;
 
